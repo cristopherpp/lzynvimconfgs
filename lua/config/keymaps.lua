66db9b5797c6b8247becd1c692bb7b916837df6c
cristopher <cristoperesc@gmail.com>
2025-08-16 14:25:38 -0500

    keymaps latin-la updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,14 @@
 
 local map = vim.keymap.set
 
+-- Toggle Terminal
 map({ "n", "t" }, "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+
+-- Select all
+map("n", "<leader>sa", "ggVG", { desc = "Select all" })
+
+-- Copy selection to clipboard (normal and visual mode)
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
+
+-- Paste from clipboard (normal and insert mode)
+map({ "n", "i" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
